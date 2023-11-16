3b1af853f55f2b25124a6f62ff870d06a98b0fe9
Eduardo Kitamura Manrique <eduardo.manrique@gmail.com>
2023-11-16 18:29:02 +0000

    Redesign the main page to include update and delete buttons in the book grid with improved aesthetics

--- src/BooksList.js
+++ src/BooksList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 function BooksList() {
   const [books, setBooks] = useState([]);
@@ -12,14 +13,41 @@ function BooksList() {
       .catch(error => console.error('Error fetching data: ', error));
   }, []);
 
+  const handleDelete = (bookId) => {
+    axios.delete(`http://localhost:3001/books/${bookId}`)
+      .then(() => {
+        setBooks(books.filter(book => book.id !== bookId));
+        console.log('Book deleted');
+      })
+      .catch(error => console.error('Error deleting book:', error));
+  };
+
   return (
     <div>
       <h1>Books</h1>
-      <ul>
-        {books.map(book => (
-          <li key={book.id}>{book.title} by {book.author}</li>
-        ))}
-      </ul>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Author</th>
+            <th>ISBN</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {books.map(book => (
+            <tr key={book.id}>
+              <td>{book.title}</td>
+              <td>{book.author}</td>
+              <td>{book.isbn}</td>
+              <td>
+                <Link to={`/update-book/${book.id}`}>Update</Link>
+                <button onClick={() => handleDelete(book.id)}>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
