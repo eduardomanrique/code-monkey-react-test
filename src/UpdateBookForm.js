f396bc04c5c49281059f17dc8ab59afa0502c7d1
Eduardo Kitamura Manrique <eduardo.manrique@gmail.com>
2023-11-19 15:14:37 +0000

    Prepopulate form fields in UpdateBookForm with current book values

--- src/UpdateBookForm.js
+++ src/UpdateBookForm.js
@@ -1,16 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 
 function UpdateBookForm() {
   const { bookId } = useParams();
-  const [title, setTitle] = useState('');
-  const [author, setAuthor] = useState('');
-  const [isbn, setIsbn] = useState('');
+  const [book, setBook] = useState({ title: '', author: '', isbn: '' });
+
+  useEffect(() => {
+    axios.get(`http://localhost:3001/books/${bookId}`)
+      .then(response => {
+        setBook(response.data);
+      })
+      .catch(error => console.error('Error fetching book:', error));
+  }, [bookId]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    axios.put(`http://localhost:3001/books/${bookId}`, { title, author, isbn })
+    axios.put(`http://localhost:3001/books/${bookId}`, book)
       .then(response => {
         console.log('Book updated:', response.data);
       })
@@ -20,13 +26,13 @@ function UpdateBookForm() {
   return (
     <form onSubmit={handleSubmit}>
       <label>Title:
-        <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+        <input type="text" value={book.title} onChange={(e) => setBook({ ...book, title: e.target.value })} />
       </label>
       <label>Author:
-        <input type="text" value={author} onChange={(e) => setAuthor(e.target.value)} />
+        <input type="text" value={book.author} onChange={(e) => setBook({ ...book, author: e.target.value })} />
       </label>
       <label>ISBN:
-        <input type="text" value={isbn} onChange={(e) => setIsbn(e.target.value)} />
+        <input type="text" value={book.isbn} onChange={(e) => setBook({ ...book, isbn: e.target.value })} />
       </label>
       <button type="submit">Update Book</button>
     </form>
