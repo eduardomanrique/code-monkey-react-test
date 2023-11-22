7ebd96a33884154c8ba4553f404006dcf5f3fe2c
Eduardo Kitamura Manrique <eduardo.manrique@gmail.com>
2023-11-22 12:06:35 +0000

    Create a new home page with a nice design

---
+++ src/Home.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div style={homeStyle}>
+      <h1>Welcome to Code Monkey's Library</h1>
+      <p>Explore a world of knowledge and adventure.</p>
+      <div style={imageContainerStyle}>
+        <img src='/path-to-your-library-image.jpg' alt='Library' style={imageStyle} />
+      </div>
+    </div>
+  );
+}
+
+const homeStyle = {
+  textAlign: 'center',
+  padding: '50px',
+  backgroundColor: '#f4f4f4',
+  borderRadius: '15px',
+  margin: '20px',
+  boxShadow: '0 10px 20px rgba(0,0,0,0.2)'
+};
+
+const imageContainerStyle = {
+  marginTop: '30px'
+};
+
+const imageStyle = {
+  maxWidth: '100%',
+  height: 'auto',
+  borderRadius: '10px'
+};
+
+export default Home;
